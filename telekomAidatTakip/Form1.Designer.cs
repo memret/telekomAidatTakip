731e4448fef42de6b54a636476c7aeeda2e95459
Mehmet Emre TURHAN <mehmetemre.turhan@gazi.edu.tr>
2017-06-15 21:54:20 +0300

    test

--- telekomAidatTakip/Form1.Designer.cs
+++ telekomAidatTakip/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 52);
+            this.button1.Location = new System.Drawing.Point(78, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
