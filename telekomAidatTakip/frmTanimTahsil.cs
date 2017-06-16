03b732c892b7649c7cdd239ddf95ff7f212fbf26
akgulonur <akgulonur6@gmail.com>
2017-06-16 15:59:14 +0300

    Revert "tasarımı yapıldı"
    This reverts commit b8ebf179792a163c0f3fd4724c1cb107ce592c01.

--- telekomAidatTakip/frmTanimTahsil.cs
+++ telekomAidatTakip/frmTanimTahsil.cs
@@ -16,20 +16,5 @@ namespace telekomAidatTakip
         {
             InitializeComponent();
         }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tahsilNo_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
