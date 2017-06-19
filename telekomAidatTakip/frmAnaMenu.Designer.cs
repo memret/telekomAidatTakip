a08ae06fa6dd9a5f8704e3333538b5102bb22c4d
Mehmet Emre TURHAN <dcfromlife.emre@gmail.com>
2017-06-19 12:37:36 +0300

    geçici anamenü

---
+++ telekomAidatTakip/frmAnaMenu.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace telekomAidatTakip
+{
+    partial class frmAnaMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.üyeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.üyeFormuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tanımlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ilTanımıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.üyeToolStripMenuItem,
+            this.tanımlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(862, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // üyeToolStripMenuItem
+            // 
+            this.üyeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.üyeFormuToolStripMenuItem});
+            this.üyeToolStripMenuItem.Name = "üyeToolStripMenuItem";
+            this.üyeToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.üyeToolStripMenuItem.Text = "Üye";
+            // 
+            // üyeFormuToolStripMenuItem
+            // 
+            this.üyeFormuToolStripMenuItem.Name = "üyeFormuToolStripMenuItem";
+            this.üyeFormuToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.üyeFormuToolStripMenuItem.Text = "Üye Formu";
+            // 
+            // tanımlarToolStripMenuItem
+            // 
+            this.tanımlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ilTanımıToolStripMenuItem});
+            this.tanımlarToolStripMenuItem.Name = "tanımlarToolStripMenuItem";
+            this.tanımlarToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tanımlarToolStripMenuItem.Text = "Tanımlar";
+            // 
+            // ilTanımıToolStripMenuItem
+            // 
+            this.ilTanımıToolStripMenuItem.Name = "ilTanımıToolStripMenuItem";
+            this.ilTanımıToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ilTanımıToolStripMenuItem.Text = "İl Tanımı";
+            this.ilTanımıToolStripMenuItem.Click += new System.EventHandler(this.ilTanımıToolStripMenuItem_Click);
+            // 
+            // frmAnaMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 323);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAnaMenu";
+            this.Text = "frmAnaMenu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem üyeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem üyeFormuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tanımlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ilTanımıToolStripMenuItem;
+    }
+}
---
+++ telekomAidatTakip/frmAnaMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace telekomAidatTakip
+{
+    public partial class frmAnaMenu : Form
+    {
+        public frmAnaMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ilTanımıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmTanimIl tanimIl = new frmTanimIl();
+            tanimIl.MdiParent = this;
+            tanimIl.Show();
+        }
+    }
+}
