e8a342d96cd4aa28132d49eb32b9142adc830fd9
Mehmet Emre TURHAN <dcfromlife.emre@gmail.com>
2017-06-20 11:44:15 +0300

    üye formu

--- telekomAidatTakip/frmUye.Designer.cs
+++ telekomAidatTakip/frmUye.Designer.cs
@@ -28,11 +28,363 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSilinme = new System.Windows.Forms.Label();
+            this.dateKayit = new System.Windows.Forms.DateTimePicker();
+            this.dateGiris = new System.Windows.Forms.DateTimePicker();
+            this.cboxUyelikTipi = new System.Windows.Forms.ComboBox();
+            this.cboxBirim = new System.Windows.Forms.ComboBox();
+            this.cboxMudurluk = new System.Windows.Forms.ComboBox();
+            this.cboxIl = new System.Windows.Forms.ComboBox();
+            this.cboxUnvan = new System.Windows.Forms.ComboBox();
+            this.cboxTahsil = new System.Windows.Forms.ComboBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtSicilNo = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tabControl1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(808, 432);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Üye Bilgileri";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(125, 19);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(677, 365);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.label11);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lblSilinme);
+            this.tabPage1.Controls.Add(this.dateKayit);
+            this.tabPage1.Controls.Add(this.dateGiris);
+            this.tabPage1.Controls.Add(this.cboxUyelikTipi);
+            this.tabPage1.Controls.Add(this.cboxBirim);
+            this.tabPage1.Controls.Add(this.cboxMudurluk);
+            this.tabPage1.Controls.Add(this.cboxIl);
+            this.tabPage1.Controls.Add(this.cboxUnvan);
+            this.tabPage1.Controls.Add(this.cboxTahsil);
+            this.tabPage1.Controls.Add(this.txtAdSoyad);
+            this.tabPage1.Controls.Add(this.txtSicilNo);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(669, 339);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Temel Bilgiler";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(26, 284);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(35, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "label1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(26, 258);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "label1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "label1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "label1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 231);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tahsil Durumu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sicil No:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Üye No:";
+            // 
+            // lblSilinme
+            // 
+            this.lblSilinme.AutoSize = true;
+            this.lblSilinme.Location = new System.Drawing.Point(127, 284);
+            this.lblSilinme.Name = "lblSilinme";
+            this.lblSilinme.Size = new System.Drawing.Size(55, 13);
+            this.lblSilinme.TabIndex = 3;
+            this.lblSilinme.Text = "bla bla bla";
+            // 
+            // dateKayit
+            // 
+            this.dateKayit.Location = new System.Drawing.Point(125, 257);
+            this.dateKayit.Name = "dateKayit";
+            this.dateKayit.Size = new System.Drawing.Size(200, 20);
+            this.dateKayit.TabIndex = 2;
+            // 
+            // dateGiris
+            // 
+            this.dateGiris.Location = new System.Drawing.Point(125, 231);
+            this.dateGiris.Name = "dateGiris";
+            this.dateGiris.Size = new System.Drawing.Size(200, 20);
+            this.dateGiris.TabIndex = 2;
+            // 
+            // cboxUyelikTipi
+            // 
+            this.cboxUyelikTipi.FormattingEnabled = true;
+            this.cboxUyelikTipi.Location = new System.Drawing.Point(125, 203);
+            this.cboxUyelikTipi.Name = "cboxUyelikTipi";
+            this.cboxUyelikTipi.Size = new System.Drawing.Size(121, 21);
+            this.cboxUyelikTipi.TabIndex = 1;
+            // 
+            // cboxBirim
+            // 
+            this.cboxBirim.FormattingEnabled = true;
+            this.cboxBirim.Location = new System.Drawing.Point(125, 176);
+            this.cboxBirim.Name = "cboxBirim";
+            this.cboxBirim.Size = new System.Drawing.Size(121, 21);
+            this.cboxBirim.TabIndex = 1;
+            // 
+            // cboxMudurluk
+            // 
+            this.cboxMudurluk.FormattingEnabled = true;
+            this.cboxMudurluk.Location = new System.Drawing.Point(125, 149);
+            this.cboxMudurluk.Name = "cboxMudurluk";
+            this.cboxMudurluk.Size = new System.Drawing.Size(121, 21);
+            this.cboxMudurluk.TabIndex = 1;
+            // 
+            // cboxIl
+            // 
+            this.cboxIl.FormattingEnabled = true;
+            this.cboxIl.Location = new System.Drawing.Point(125, 122);
+            this.cboxIl.Name = "cboxIl";
+            this.cboxIl.Size = new System.Drawing.Size(121, 21);
+            this.cboxIl.TabIndex = 1;
+            // 
+            // cboxUnvan
+            // 
+            this.cboxUnvan.FormattingEnabled = true;
+            this.cboxUnvan.Location = new System.Drawing.Point(125, 95);
+            this.cboxUnvan.Name = "cboxUnvan";
+            this.cboxUnvan.Size = new System.Drawing.Size(121, 21);
+            this.cboxUnvan.TabIndex = 1;
+            // 
+            // cboxTahsil
+            // 
+            this.cboxTahsil.FormattingEnabled = true;
+            this.cboxTahsil.Location = new System.Drawing.Point(125, 68);
+            this.cboxTahsil.Name = "cboxTahsil";
+            this.cboxTahsil.Size = new System.Drawing.Size(121, 21);
+            this.cboxTahsil.TabIndex = 1;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(125, 42);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(218, 20);
+            this.txtAdSoyad.TabIndex = 0;
+            // 
+            // txtSicilNo
+            // 
+            this.txtSicilNo.Location = new System.Drawing.Point(125, 16);
+            this.txtSicilNo.Name = "txtSicilNo";
+            this.txtSicilNo.Size = new System.Drawing.Size(100, 20);
+            this.txtSicilNo.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(669, 339);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Adres Bilgileri";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(669, 339);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Nüfus Bilgileri";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(669, 339);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Aidat Bilgileri";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(669, 339);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Not";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // frmUye
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 478);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmUye";
             this.Text = "frmUye";
+            this.Load += new System.EventHandler(this.frmUye_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSilinme;
+        private System.Windows.Forms.DateTimePicker dateKayit;
+        private System.Windows.Forms.DateTimePicker dateGiris;
+        private System.Windows.Forms.ComboBox cboxUyelikTipi;
+        private System.Windows.Forms.ComboBox cboxBirim;
+        private System.Windows.Forms.ComboBox cboxMudurluk;
+        private System.Windows.Forms.ComboBox cboxIl;
+        private System.Windows.Forms.ComboBox cboxUnvan;
+        private System.Windows.Forms.ComboBox cboxTahsil;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtSicilNo;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
     }
 }
--- telekomAidatTakip/frmUye.cs
+++ telekomAidatTakip/frmUye.cs
@@ -16,5 +16,75 @@ namespace telekomAidatTakip
         {
             InitializeComponent();
         }
+
+        
+
+        private void frmUye_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DoldurTemelBilgiler(string sicilno)
+        {
+            Database db = new Database();
+            var data = db.DataOku("select * from uye where sicilno=@0",sicilno);
+            if (data.Read())
+            {
+                txtAdSoyad.Text = data["adsoyad"].ToString();
+                txtSicilNo.Text = sicilno;
+
+            }
+        }
+        private void DoldurTahsil()
+        {
+            Database db = new Database();
+            var data = db.DataOku("select * from tahsil");
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            while(data.Read())
+                cboxSource.Add(Convert.ToInt32(data["tahsilno"]), data["tahsiladi"].ToString());
+            cboxTahsil.DataSource = new BindingSource(cboxSource, null);
+            cboxTahsil.DisplayMember = "Value";
+            cboxTahsil.ValueMember = "Key";
+        }
+        private void DoldurUnvan()
+        {
+            Database db = new Database();
+            var data = db.DataOku("select * from unvan");
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            while (data.Read())
+                cboxSource.Add(Convert.ToInt32(data["unvanNo"]), data["Unvanadi"].ToString());
+            cboxUnvan.DataSource = new BindingSource(cboxSource, null);
+            cboxUnvan.DisplayMember = "Value";
+            cboxUnvan.ValueMember = "Key";
+        }
+        private void DoldurIl()
+        {
+            Database db = new Database();
+            var data = db.DataOku("select * from il");
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            while (data.Read())
+                cboxSource.Add(Convert.ToInt32(data["ilno"]), data["iladi"].ToString());
+            cboxIl.DataSource = new BindingSource(cboxSource, null);
+            cboxIl.DisplayMember = "Value";
+            cboxIl.ValueMember = "Key";
+        }
+        private void DoldurMudurluk()
+        {
+            Database db = new Database();
+            var data = db.DataOku("select * from mudurluk");
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            while (data.Read())
+                cboxSource.Add(Convert.ToInt32(data["mudurlukno"]), data["mudurlukadi"].ToString());
+            cboxIl.DataSource = new BindingSource(cboxSource, null);
+            cboxIl.DisplayMember = "Value";
+            cboxIl.ValueMember = "Key";
+        }
+        private void DoldurBirim()
+        {
+
+        }
+        private void DoldurUyelikTipi()
+        {
+
+        }
     }
 }
