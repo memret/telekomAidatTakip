22bfe051ae63928c82348eeabfb898e899b0d5f2
Omer TANIS <omertanis123@gmail.com>
2017-06-20 12:50:45 +0300

    FrmAidatToplu comboBoxlar dolduruldu.

--- telekomAidatTakip/frmAidatToplu.Designer.cs
+++ telekomAidatTakip/frmAidatToplu.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.cboxil = new System.Windows.Forms.ComboBox();
-            this.cboxMudurluk = new System.Windows.Forms.ComboBox();
-            this.cboxKisim = new System.Windows.Forms.ComboBox();
-            this.cboxYil = new System.Windows.Forms.ComboBox();
-            this.cboxAy = new System.Windows.Forms.ComboBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.txtAidatMiktari = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cboxAy = new System.Windows.Forms.ComboBox();
+            this.cboxYil = new System.Windows.Forms.ComboBox();
+            this.cboxKisim = new System.Windows.Forms.ComboBox();
+            this.cboxMudurluk = new System.Windows.Forms.ComboBox();
+            this.cboxil = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnKaydet = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -61,141 +61,159 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(29, 31);
+            this.groupBox1.Location = new System.Drawing.Point(26, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(562, 367);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(500, 294);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Toplu Aidat Girişi";
             // 
-            // groupBox2
+            // txtAidatMiktari
             // 
-            this.groupBox2.Controls.Add(this.btnKaydet);
-            this.groupBox2.Location = new System.Drawing.Point(29, 429);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(562, 156);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 71);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(21, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "İl:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 121);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Müdürlük:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 171);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Kısım:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 221);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 20);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Aidat Yılı ve Ayı:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 271);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 20);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Aidat Miktarı:";
-            // 
-            // cboxil
-            // 
-            this.cboxil.FormattingEnabled = true;
-            this.cboxil.Location = new System.Drawing.Point(147, 68);
-            this.cboxil.Name = "cboxil";
-            this.cboxil.Size = new System.Drawing.Size(221, 28);
-            this.cboxil.TabIndex = 6;
-            // 
-            // cboxMudurluk
-            // 
-            this.cboxMudurluk.FormattingEnabled = true;
-            this.cboxMudurluk.Location = new System.Drawing.Point(147, 121);
-            this.cboxMudurluk.Name = "cboxMudurluk";
-            this.cboxMudurluk.Size = new System.Drawing.Size(221, 28);
-            this.cboxMudurluk.TabIndex = 7;
-            // 
-            // cboxKisim
-            // 
-            this.cboxKisim.FormattingEnabled = true;
-            this.cboxKisim.Location = new System.Drawing.Point(147, 171);
-            this.cboxKisim.Name = "cboxKisim";
-            this.cboxKisim.Size = new System.Drawing.Size(221, 28);
-            this.cboxKisim.TabIndex = 8;
-            // 
-            // cboxYil
-            // 
-            this.cboxYil.FormattingEnabled = true;
-            this.cboxYil.Location = new System.Drawing.Point(147, 221);
-            this.cboxYil.Name = "cboxYil";
-            this.cboxYil.Size = new System.Drawing.Size(153, 28);
-            this.cboxYil.TabIndex = 9;
-            // 
-            // cboxAy
-            // 
-            this.cboxAy.FormattingEnabled = true;
-            this.cboxAy.Location = new System.Drawing.Point(362, 218);
-            this.cboxAy.Name = "cboxAy";
-            this.cboxAy.Size = new System.Drawing.Size(163, 28);
-            this.cboxAy.TabIndex = 10;
+            this.txtAidatMiktari.Location = new System.Drawing.Point(131, 214);
+            this.txtAidatMiktari.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAidatMiktari.Name = "txtAidatMiktari";
+            this.txtAidatMiktari.Size = new System.Drawing.Size(136, 22);
+            this.txtAidatMiktari.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(320, 221);
+            this.label8.Location = new System.Drawing.Point(284, 177);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(13, 20);
+            this.label8.Size = new System.Drawing.Size(12, 17);
             this.label8.TabIndex = 13;
             this.label8.Text = "/";
             // 
-            // txtAidatMiktari
+            // cboxAy
             // 
-            this.txtAidatMiktari.Location = new System.Drawing.Point(147, 268);
-            this.txtAidatMiktari.Name = "txtAidatMiktari";
-            this.txtAidatMiktari.Size = new System.Drawing.Size(153, 26);
-            this.txtAidatMiktari.TabIndex = 14;
+            this.cboxAy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxAy.FormattingEnabled = true;
+            this.cboxAy.Location = new System.Drawing.Point(322, 174);
+            this.cboxAy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboxAy.Name = "cboxAy";
+            this.cboxAy.Size = new System.Drawing.Size(145, 24);
+            this.cboxAy.TabIndex = 10;
+            // 
+            // cboxYil
+            // 
+            this.cboxYil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxYil.FormattingEnabled = true;
+            this.cboxYil.Location = new System.Drawing.Point(131, 177);
+            this.cboxYil.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboxYil.Name = "cboxYil";
+            this.cboxYil.Size = new System.Drawing.Size(136, 24);
+            this.cboxYil.TabIndex = 9;
+            // 
+            // cboxKisim
+            // 
+            this.cboxKisim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxKisim.FormattingEnabled = true;
+            this.cboxKisim.Location = new System.Drawing.Point(131, 137);
+            this.cboxKisim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboxKisim.Name = "cboxKisim";
+            this.cboxKisim.Size = new System.Drawing.Size(197, 24);
+            this.cboxKisim.TabIndex = 8;
+            // 
+            // cboxMudurluk
+            // 
+            this.cboxMudurluk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxMudurluk.FormattingEnabled = true;
+            this.cboxMudurluk.Location = new System.Drawing.Point(131, 97);
+            this.cboxMudurluk.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboxMudurluk.Name = "cboxMudurluk";
+            this.cboxMudurluk.Size = new System.Drawing.Size(197, 24);
+            this.cboxMudurluk.TabIndex = 7;
+            // 
+            // cboxil
+            // 
+            this.cboxil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxil.FormattingEnabled = true;
+            this.cboxil.Location = new System.Drawing.Point(131, 54);
+            this.cboxil.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboxil.Name = "cboxil";
+            this.cboxil.Size = new System.Drawing.Size(197, 24);
+            this.cboxil.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Aidat Miktarı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Aidat Yılı ve Ayı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kısım:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Müdürlük:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İl:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnKaydet);
+            this.groupBox2.Location = new System.Drawing.Point(26, 343);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(500, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
             // 
             // btnKaydet
             // 
-            this.btnKaydet.Location = new System.Drawing.Point(211, 44);
+            this.btnKaydet.Location = new System.Drawing.Point(188, 35);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnKaydet.Name = "btnKaydet";
-            this.btnKaydet.Size = new System.Drawing.Size(132, 81);
+            this.btnKaydet.Size = new System.Drawing.Size(117, 65);
             this.btnKaydet.TabIndex = 0;
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.UseVisualStyleBackColor = true;
             // 
             // frmAidatToplu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 589);
+            this.ClientSize = new System.Drawing.Size(549, 471);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmAidatToplu";
             this.Text = "frmAidatToplu";
+            this.Load += new System.EventHandler(this.frmAidatToplu_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
--- telekomAidatTakip/frmAidatToplu.cs
+++ telekomAidatTakip/frmAidatToplu.cs
@@ -16,5 +16,82 @@ namespace telekomAidatTakip
         {
             InitializeComponent();
         }
+
+        private void frmAidatToplu_Load(object sender, EventArgs e)
+        {
+            cBoxIlDoldur();
+            cBoxMudurlukDoldur();
+            cBoxKisimDoldur();
+            cBoxYilDoldur();
+            cBoxAyDoldur();
+
+
+        }
+
+        //iller cbox doldurma fonksiyonu
+        private void cBoxIlDoldur()
+        {
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            Database db2 = new Database();
+            var data = db2.DataOku("SELECT ilNo,ilAdi FROM il");
+
+            while (data.Read())
+            {
+                cboxSource.Add(Convert.ToInt32(data["ilNo"]), data["ilAdi"].ToString());
+            }
+
+            cboxil.DataSource = new BindingSource(cboxSource, null);
+            cboxil.DisplayMember = "Value";
+            cboxil.ValueMember = "Key";
+        }
+
+        //mudurluk cbox doldurma fonksiyonu
+        private void cBoxMudurlukDoldur()
+        {
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            Database db2 = new Database();
+            var data = db2.DataOku("SELECT mudurlukNo,mudurlukAdi FROM mudurluk");
+
+            while (data.Read())
+            {
+                cboxSource.Add(Convert.ToInt32(data["mudurlukNo"]), data["mudurlukAdi"].ToString());
+            }
+            cboxMudurluk.DataSource = new BindingSource(cboxSource, null);
+            cboxMudurluk.DisplayMember = "Value";
+            cboxMudurluk.ValueMember = "Key";
+
+        }
+
+        private void cBoxKisimDoldur()
+        {
+            Dictionary<int, string> cboxSource = new Dictionary<int, string>();
+            Database db2 = new Database();
+            var data = db2.DataOku("SELECT birimNo,birimAdi FROM Birim");
+
+            while (data.Read())
+            {
+                cboxSource.Add(Convert.ToInt32(data["birimNo"]), data["birimAdi"].ToString());
+            }
+            cboxKisim.DataSource = new BindingSource(cboxSource, null);
+            cboxKisim.DisplayMember = "Value";
+            cboxKisim.ValueMember = "Key";
+        }
+        private void cBoxYilDoldur()
+        {
+            for (int i = 1950; i <= 2017; i++)
+            {
+                cboxYil.Items.Add(i);
+            }
+        }
+
+        private void cBoxAyDoldur()
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                cboxAy.Items.Add(i);
+            }
+        }
+
+
     }
 }
