33e976c6b3516c3d51877b14b78a9ca1714a9c7b
burcuppo <burcu.polatt@outlook.com>
2017-06-21 11:17:27 +0300

    Rapor il

--- telekomAidatTakip/frmRaporil.Designer.cs
+++ telekomAidatTakip/frmRaporil.Designer.cs
@@ -28,11 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gboxListelemeİslemi = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEkranaListele = new System.Windows.Forms.Button();
+            this.btnYazdir = new System.Windows.Forms.Button();
+            this.gboxBulunanKayitlar = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ListSiraNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Listİl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ListAktif = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ListPasif = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ListToplam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gboxListelemeİslemi.SuspendLayout();
+            this.gboxBulunanKayitlar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gboxListelemeİslemi
+            // 
+            this.gboxListelemeİslemi.Controls.Add(this.btnYazdir);
+            this.gboxListelemeİslemi.Controls.Add(this.btnEkranaListele);
+            this.gboxListelemeİslemi.Controls.Add(this.label1);
+            this.gboxListelemeİslemi.Location = new System.Drawing.Point(0, 0);
+            this.gboxListelemeİslemi.Name = "gboxListelemeİslemi";
+            this.gboxListelemeİslemi.Size = new System.Drawing.Size(564, 100);
+            this.gboxListelemeİslemi.TabIndex = 0;
+            this.gboxListelemeİslemi.TabStop = false;
+            this.gboxListelemeİslemi.Text = "Listeleme İşlemi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İllere Göre Üye Dağılım Listesi";
+            // 
+            // btnEkranaListele
+            // 
+            this.btnEkranaListele.Location = new System.Drawing.Point(396, 68);
+            this.btnEkranaListele.Name = "btnEkranaListele";
+            this.btnEkranaListele.Size = new System.Drawing.Size(83, 31);
+            this.btnEkranaListele.TabIndex = 1;
+            this.btnEkranaListele.Text = "Ekrana Listele";
+            this.btnEkranaListele.UseVisualStyleBackColor = true;
+            this.btnEkranaListele.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.Location = new System.Drawing.Point(485, 68);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(78, 31);
+            this.btnYazdir.TabIndex = 2;
+            this.btnYazdir.Text = "Yazdır";
+            this.btnYazdir.UseVisualStyleBackColor = true;
+            // 
+            // gboxBulunanKayitlar
+            // 
+            this.gboxBulunanKayitlar.Controls.Add(this.listView1);
+            this.gboxBulunanKayitlar.Location = new System.Drawing.Point(3, 124);
+            this.gboxBulunanKayitlar.Name = "gboxBulunanKayitlar";
+            this.gboxBulunanKayitlar.Size = new System.Drawing.Size(690, 279);
+            this.gboxBulunanKayitlar.TabIndex = 1;
+            this.gboxBulunanKayitlar.TabStop = false;
+            this.gboxBulunanKayitlar.Text = "Bulunan Kayıtlar";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ListSiraNo,
+            this.Listİl,
+            this.ListAktif,
+            this.ListPasif,
+            this.ListToplam});
+            this.listView1.Location = new System.Drawing.Point(3, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(687, 263);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ListSiraNo
+            // 
+            this.ListSiraNo.Text = "S.N";
+            this.ListSiraNo.Width = 80;
+            // 
+            // Listİl
+            // 
+            this.Listİl.Text = "İli";
+            this.Listİl.Width = 99;
+            // 
+            // ListAktif
+            // 
+            this.ListAktif.Text = "Aktif";
+            this.ListAktif.Width = 86;
+            // 
+            // ListPasif
+            // 
+            this.ListPasif.Text = "Pasif";
+            this.ListPasif.Width = 87;
+            // 
+            // ListToplam
+            // 
+            this.ListToplam.Text = "Toplam";
+            this.ListToplam.Width = 92;
+            // 
+            // frmRaporIl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 406);
+            this.Controls.Add(this.gboxBulunanKayitlar);
+            this.Controls.Add(this.gboxListelemeİslemi);
+            this.Name = "frmRaporIl";
             this.Text = "frmRaporil";
+            this.gboxListelemeİslemi.ResumeLayout(false);
+            this.gboxListelemeİslemi.PerformLayout();
+            this.gboxBulunanKayitlar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gboxListelemeİslemi;
+        private System.Windows.Forms.Button btnYazdir;
+        private System.Windows.Forms.Button btnEkranaListele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gboxBulunanKayitlar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ListSiraNo;
+        private System.Windows.Forms.ColumnHeader Listİl;
+        private System.Windows.Forms.ColumnHeader ListAktif;
+        private System.Windows.Forms.ColumnHeader ListPasif;
+        private System.Windows.Forms.ColumnHeader ListToplam;
     }
 }
