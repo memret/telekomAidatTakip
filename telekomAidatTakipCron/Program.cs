1c8f1f756081d84179f7d4424c1cbc09174e1c33
Mehmet Emre TURHAN <dcfromlife.emre@gmail.com>
2017-07-04 09:32:58 +0300

    şablon

--- telekomAidatTakipCron/Program.cs
+++ telekomAidatTakipCron/Program.cs
@@ -14,11 +14,16 @@ namespace telekomAidatTakipCron
         }
         void MailTopluGonder(string[] mailListesi, string baslik, string icerik)
         {
-
+            object smtp = "";
+            //smtp tanımlanacak ayarları yapılacak burada belki veritabanından çekilebilir
+            foreach (string mail in mailListesi)
+            {
+                MailTekliGonder(smtp,mail, baslik, icerik);
+            }
         }
         void MailTekliGonder(object smtpZimbirtisi, string mail, string baslik, string icerik)
         {
-
+            //smtp.send gibi birsey ile gönderilecek
         }
     }
 }
