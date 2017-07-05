bc43516cd070ed7d8e1c495f8c154c0c680a5b76
ybeyza <ybeyza61@gmail.com>
2017-07-05 15:39:46 +0300

    görüntü

--- telekomAidatTakip/frmAidatMiktar.Designer.cs
+++ telekomAidatTakip/frmAidatMiktar.Designer.cs
@@ -49,36 +49,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 28);
+            this.label1.Location = new System.Drawing.Point(28, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.Size = new System.Drawing.Size(21, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "İl:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 56);
+            this.label2.Location = new System.Drawing.Point(28, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.Size = new System.Drawing.Size(78, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Müdürlük:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 81);
+            this.label3.Location = new System.Drawing.Point(28, 125);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.Size = new System.Drawing.Size(48, 20);
             this.label3.TabIndex = 0;
             this.label3.Text = "Birim:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 108);
+            this.label4.Location = new System.Drawing.Point(28, 166);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 1;
             this.label4.Text = "Aidat Miktarı:";
             // 
@@ -86,9 +90,10 @@
             // 
             this.cboxIl.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboxIl.FormattingEnabled = true;
-            this.cboxIl.Location = new System.Drawing.Point(101, 28);
+            this.cboxIl.Location = new System.Drawing.Point(152, 43);
+            this.cboxIl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboxIl.Name = "cboxIl";
-            this.cboxIl.Size = new System.Drawing.Size(238, 21);
+            this.cboxIl.Size = new System.Drawing.Size(355, 28);
             this.cboxIl.TabIndex = 0;
             this.cboxIl.SelectionChangeCommitted += new System.EventHandler(this.cboxIl_SelectionChangeCommitted);
             // 
@@ -96,9 +101,10 @@
             // 
             this.cboxMudurluk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboxMudurluk.FormattingEnabled = true;
-            this.cboxMudurluk.Location = new System.Drawing.Point(101, 53);
+            this.cboxMudurluk.Location = new System.Drawing.Point(152, 82);
+            this.cboxMudurluk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboxMudurluk.Name = "cboxMudurluk";
-            this.cboxMudurluk.Size = new System.Drawing.Size(238, 21);
+            this.cboxMudurluk.Size = new System.Drawing.Size(355, 28);
             this.cboxMudurluk.TabIndex = 1;
             this.cboxMudurluk.SelectionChangeCommitted += new System.EventHandler(this.cboxMudurluk_SelectionChangeCommitted);
             // 
@@ -106,17 +112,19 @@
             // 
             this.cboxBirim.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboxBirim.FormattingEnabled = true;
-            this.cboxBirim.Location = new System.Drawing.Point(101, 78);
+            this.cboxBirim.Location = new System.Drawing.Point(152, 120);
+            this.cboxBirim.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cboxBirim.Name = "cboxBirim";
-            this.cboxBirim.Size = new System.Drawing.Size(238, 21);
+            this.cboxBirim.Size = new System.Drawing.Size(355, 28);
             this.cboxBirim.TabIndex = 2;
             this.cboxBirim.SelectionChangeCommitted += new System.EventHandler(this.cboxBirim_SelectionChangeCommitted);
             // 
             // txtAidatMiktari
             // 
-            this.txtAidatMiktari.Location = new System.Drawing.Point(101, 105);
+            this.txtAidatMiktari.Location = new System.Drawing.Point(152, 162);
+            this.txtAidatMiktari.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtAidatMiktari.Name = "txtAidatMiktari";
-            this.txtAidatMiktari.Size = new System.Drawing.Size(175, 20);
+            this.txtAidatMiktari.Size = new System.Drawing.Size(260, 26);
             this.txtAidatMiktari.TabIndex = 3;
             // 
             // grpbxAidatMiktar
@@ -135,9 +143,11 @@
             this.grpbxAidatMiktar.Controls.Add(this.label3);
             this.grpbxAidatMiktar.Controls.Add(this.label2);
             this.grpbxAidatMiktar.Controls.Add(this.label1);
-            this.grpbxAidatMiktar.Location = new System.Drawing.Point(12, 12);
+            this.grpbxAidatMiktar.Location = new System.Drawing.Point(18, 18);
+            this.grpbxAidatMiktar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grpbxAidatMiktar.Name = "grpbxAidatMiktar";
-            this.grpbxAidatMiktar.Size = new System.Drawing.Size(364, 208);
+            this.grpbxAidatMiktar.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grpbxAidatMiktar.Size = new System.Drawing.Size(546, 320);
             this.grpbxAidatMiktar.TabIndex = 0;
             this.grpbxAidatMiktar.TabStop = false;
             this.grpbxAidatMiktar.Text = "Aidat Miktarı Değiştirme";
@@ -145,34 +155,38 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(282, 108);
+            this.label7.Location = new System.Drawing.Point(423, 166);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.Size = new System.Drawing.Size(27, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "TL";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(101, 132);
+            this.label6.Location = new System.Drawing.Point(152, 203);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.Size = new System.Drawing.Size(0, 20);
             this.label6.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(129, 134);
+            this.label5.Location = new System.Drawing.Point(194, 206);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.Size = new System.Drawing.Size(0, 20);
             this.label5.TabIndex = 4;
             // 
             // btnIptal
             // 
-            this.btnIptal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnIptal.Location = new System.Drawing.Point(217, 141);
+            this.btnIptal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btnIptal.Location = new System.Drawing.Point(326, 217);
+            this.btnIptal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIptal.Name = "btnIptal";
-            this.btnIptal.Size = new System.Drawing.Size(75, 50);
+            this.btnIptal.Size = new System.Drawing.Size(112, 77);
             this.btnIptal.TabIndex = 5;
             this.btnIptal.Text = "İptal";
             this.btnIptal.UseVisualStyleBackColor = true;
@@ -180,10 +194,11 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(110, 141);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button1.Location = new System.Drawing.Point(165, 217);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.Size = new System.Drawing.Size(112, 77);
             this.button1.TabIndex = 4;
             this.button1.Text = "Kaydet";
             this.button1.UseVisualStyleBackColor = true;
@@ -192,9 +207,10 @@
             // btnKaydet
             // 
             this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnKaydet.Location = new System.Drawing.Point(110, 141);
+            this.btnKaydet.Location = new System.Drawing.Point(165, 217);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnKaydet.Name = "btnKaydet";
-            this.btnKaydet.Size = new System.Drawing.Size(75, 50);
+            this.btnKaydet.Size = new System.Drawing.Size(112, 77);
             this.btnKaydet.TabIndex = 1;
             this.btnKaydet.Text = "Güncelle";
             this.btnKaydet.UseVisualStyleBackColor = true;
@@ -202,11 +218,12 @@
             // 
             // frmAidatMiktar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 233);
+            this.ClientSize = new System.Drawing.Size(588, 358);
             this.Controls.Add(this.grpbxAidatMiktar);
-            this.MinimumSize = new System.Drawing.Size(408, 272);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(601, 388);
             this.Name = "frmAidatMiktar";
             this.Text = "Aidat Miktarı Belirleme";
             this.Load += new System.EventHandler(this.frmAidatMiktar_Load);
