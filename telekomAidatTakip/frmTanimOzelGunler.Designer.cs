13a577763084039ff4e9b6912fe06cbff3c7a08a
niirdm <nehir.erdem@ug.bilkent.edu.tr>
2017-07-05 16:31:59 +0300

    frmTanimOzelGunler v1

--- telekomAidatTakip/frmTanimOzelGunler.Designer.cs
+++ telekomAidatTakip/frmTanimOzelGunler.Designer.cs
@@ -29,154 +29,175 @@
         private void InitializeComponent()
         {
             this.gboxOzelGunTanimlari = new System.Windows.Forms.GroupBox();
-            this.gboxTabloGosterimi = new System.Windows.Forms.GroupBox();
-            this.lblMailBasligi = new System.Windows.Forms.Label();
-            this.lblTarih = new System.Windows.Forms.Label();
-            this.lblMesaj = new System.Windows.Forms.Label();
-            this.lblYorum = new System.Windows.Forms.Label();
-            this.TboxMailBasligi = new System.Windows.Forms.MaskedTextBox();
-            this.tboxmesaj = new System.Windows.Forms.TextBox();
-            this.tboxYorum = new System.Windows.Forms.TextBox();
-            this.dateTimeTarih = new System.Windows.Forms.DateTimePicker();
-            this.btnYeni = new System.Windows.Forms.Button();
-            this.btnKaydet = new System.Windows.Forms.Button();
             this.btnSil = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.dateTimeTarih = new System.Windows.Forms.DateTimePicker();
+            this.txtYorum = new System.Windows.Forms.TextBox();
+            this.txtMsj = new System.Windows.Forms.TextBox();
+            this.txtBaslik = new System.Windows.Forms.MaskedTextBox();
+            this.lblYorum = new System.Windows.Forms.Label();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.lblTarih = new System.Windows.Forms.Label();
+            this.lblMailBasligi = new System.Windows.Forms.Label();
+            this.gboxTabloGosterimi = new System.Windows.Forms.GroupBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtGünNo = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.gboxOzelGunTanimlari.SuspendLayout();
             this.gboxTabloGosterimi.SuspendLayout();
             this.SuspendLayout();
             // 
             // gboxOzelGunTanimlari
             // 
+            this.gboxOzelGunTanimlari.Controls.Add(this.txtGünNo);
+            this.gboxOzelGunTanimlari.Controls.Add(this.label1);
             this.gboxOzelGunTanimlari.Controls.Add(this.btnSil);
             this.gboxOzelGunTanimlari.Controls.Add(this.btnKaydet);
             this.gboxOzelGunTanimlari.Controls.Add(this.btnYeni);
             this.gboxOzelGunTanimlari.Controls.Add(this.dateTimeTarih);
-            this.gboxOzelGunTanimlari.Controls.Add(this.tboxYorum);
-            this.gboxOzelGunTanimlari.Controls.Add(this.tboxmesaj);
-            this.gboxOzelGunTanimlari.Controls.Add(this.TboxMailBasligi);
+            this.gboxOzelGunTanimlari.Controls.Add(this.txtYorum);
+            this.gboxOzelGunTanimlari.Controls.Add(this.txtMsj);
+            this.gboxOzelGunTanimlari.Controls.Add(this.txtBaslik);
             this.gboxOzelGunTanimlari.Controls.Add(this.lblYorum);
             this.gboxOzelGunTanimlari.Controls.Add(this.lblMesaj);
             this.gboxOzelGunTanimlari.Controls.Add(this.lblTarih);
             this.gboxOzelGunTanimlari.Controls.Add(this.lblMailBasligi);
-            this.gboxOzelGunTanimlari.Location = new System.Drawing.Point(19, 12);
+            this.gboxOzelGunTanimlari.Location = new System.Drawing.Point(25, 13);
+            this.gboxOzelGunTanimlari.Margin = new System.Windows.Forms.Padding(4);
             this.gboxOzelGunTanimlari.Name = "gboxOzelGunTanimlari";
-            this.gboxOzelGunTanimlari.Size = new System.Drawing.Size(543, 296);
+            this.gboxOzelGunTanimlari.Padding = new System.Windows.Forms.Padding(4);
+            this.gboxOzelGunTanimlari.Size = new System.Drawing.Size(724, 403);
             this.gboxOzelGunTanimlari.TabIndex = 0;
             this.gboxOzelGunTanimlari.TabStop = false;
             this.gboxOzelGunTanimlari.Text = "Özel Gün Tanımları";
             // 
-            // gboxTabloGosterimi
+            // btnSil
             // 
-            this.gboxTabloGosterimi.Controls.Add(this.listView1);
-            this.gboxTabloGosterimi.Location = new System.Drawing.Point(19, 314);
-            this.gboxTabloGosterimi.Name = "gboxTabloGosterimi";
-            this.gboxTabloGosterimi.Size = new System.Drawing.Size(543, 148);
-            this.gboxTabloGosterimi.TabIndex = 1;
-            this.gboxTabloGosterimi.TabStop = false;
-            this.gboxTabloGosterimi.Text = "Tablo Gösterimi";
+            this.btnSil.Location = new System.Drawing.Point(573, 355);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(111, 32);
+            this.btnSil.TabIndex = 14;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
-            // lblMailBasligi
+            // btnKaydet
             // 
-            this.lblMailBasligi.AutoSize = true;
-            this.lblMailBasligi.Location = new System.Drawing.Point(34, 34);
-            this.lblMailBasligi.Name = "lblMailBasligi";
-            this.lblMailBasligi.Size = new System.Drawing.Size(65, 13);
-            this.lblMailBasligi.TabIndex = 1;
-            this.lblMailBasligi.Text = "Mail Başlığı :";
-            this.lblMailBasligi.Click += new System.EventHandler(this.label2_Click);
+            this.btnKaydet.Location = new System.Drawing.Point(446, 356);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(119, 31);
+            this.btnKaydet.TabIndex = 13;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
-            // lblTarih
+            // btnYeni
             // 
-            this.lblTarih.AutoSize = true;
-            this.lblTarih.Location = new System.Drawing.Point(34, 69);
-            this.lblTarih.Name = "lblTarih";
-            this.lblTarih.Size = new System.Drawing.Size(37, 13);
-            this.lblTarih.TabIndex = 5;
-            this.lblTarih.Text = "Tarih :";
+            this.btnYeni.Location = new System.Drawing.Point(314, 356);
+            this.btnYeni.Margin = new System.Windows.Forms.Padding(4);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(124, 32);
+            this.btnYeni.TabIndex = 12;
+            this.btnYeni.Text = "Yeni";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
             // 
-            // lblMesaj
+            // dateTimeTarih
             // 
-            this.lblMesaj.AutoSize = true;
-            this.lblMesaj.Location = new System.Drawing.Point(34, 109);
-            this.lblMesaj.Name = "lblMesaj";
-            this.lblMesaj.Size = new System.Drawing.Size(41, 13);
-            this.lblMesaj.TabIndex = 6;
-            this.lblMesaj.Text = "Mesaj :";
+            this.dateTimeTarih.Location = new System.Drawing.Point(156, 109);
+            this.dateTimeTarih.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimeTarih.Name = "dateTimeTarih";
+            this.dateTimeTarih.Size = new System.Drawing.Size(384, 22);
+            this.dateTimeTarih.TabIndex = 10;
+            // 
+            // txtYorum
+            // 
+            this.txtYorum.Location = new System.Drawing.Point(156, 237);
+            this.txtYorum.Margin = new System.Windows.Forms.Padding(4);
+            this.txtYorum.Multiline = true;
+            this.txtYorum.Name = "txtYorum";
+            this.txtYorum.Size = new System.Drawing.Size(384, 93);
+            this.txtYorum.TabIndex = 9;
+            // 
+            // txtMsj
+            // 
+            this.txtMsj.Location = new System.Drawing.Point(156, 141);
+            this.txtMsj.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMsj.Multiline = true;
+            this.txtMsj.Name = "txtMsj";
+            this.txtMsj.Size = new System.Drawing.Size(384, 88);
+            this.txtMsj.TabIndex = 8;
+            this.txtMsj.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtBaslik
+            // 
+            this.txtBaslik.Location = new System.Drawing.Point(156, 69);
+            this.txtBaslik.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBaslik.Name = "txtBaslik";
+            this.txtBaslik.Size = new System.Drawing.Size(384, 22);
+            this.txtBaslik.TabIndex = 0;
+            this.txtBaslik.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.TboxMailBasligi_MaskInputRejected);
             // 
             // lblYorum
             // 
             this.lblYorum.AutoSize = true;
-            this.lblYorum.Location = new System.Drawing.Point(34, 160);
+            this.lblYorum.Location = new System.Drawing.Point(46, 228);
+            this.lblYorum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblYorum.Name = "lblYorum";
-            this.lblYorum.Size = new System.Drawing.Size(43, 13);
+            this.lblYorum.Size = new System.Drawing.Size(57, 17);
             this.lblYorum.TabIndex = 7;
             this.lblYorum.Text = "Yorum :";
             // 
-            // TboxMailBasligi
+            // lblMesaj
             // 
-            this.TboxMailBasligi.Location = new System.Drawing.Point(116, 31);
-            this.TboxMailBasligi.Name = "TboxMailBasligi";
-            this.TboxMailBasligi.Size = new System.Drawing.Size(289, 20);
-            this.TboxMailBasligi.TabIndex = 0;
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Location = new System.Drawing.Point(46, 165);
+            this.lblMesaj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(53, 17);
+            this.lblMesaj.TabIndex = 6;
+            this.lblMesaj.Text = "Mesaj :";
             // 
-            // tboxmesaj
+            // lblTarih
             // 
-            this.tboxmesaj.Location = new System.Drawing.Point(116, 89);
-            this.tboxmesaj.Multiline = true;
-            this.tboxmesaj.Name = "tboxmesaj";
-            this.tboxmesaj.Size = new System.Drawing.Size(289, 72);
-            this.tboxmesaj.TabIndex = 8;
-            this.tboxmesaj.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.lblTarih.AutoSize = true;
+            this.lblTarih.Location = new System.Drawing.Point(46, 116);
+            this.lblTarih.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTarih.Name = "lblTarih";
+            this.lblTarih.Size = new System.Drawing.Size(49, 17);
+            this.lblTarih.TabIndex = 5;
+            this.lblTarih.Text = "Tarih :";
             // 
-            // tboxYorum
+            // lblMailBasligi
             // 
-            this.tboxYorum.Location = new System.Drawing.Point(116, 167);
-            this.tboxYorum.Multiline = true;
-            this.tboxYorum.Name = "tboxYorum";
-            this.tboxYorum.Size = new System.Drawing.Size(289, 76);
-            this.tboxYorum.TabIndex = 9;
+            this.lblMailBasligi.AutoSize = true;
+            this.lblMailBasligi.Location = new System.Drawing.Point(46, 73);
+            this.lblMailBasligi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMailBasligi.Name = "lblMailBasligi";
+            this.lblMailBasligi.Size = new System.Drawing.Size(86, 17);
+            this.lblMailBasligi.TabIndex = 1;
+            this.lblMailBasligi.Text = "Mail Başlığı :";
+            this.lblMailBasligi.Click += new System.EventHandler(this.label2_Click);
             // 
-            // dateTimeTarih
+            // gboxTabloGosterimi
             // 
-            this.dateTimeTarih.Location = new System.Drawing.Point(116, 63);
-            this.dateTimeTarih.Name = "dateTimeTarih";
-            this.dateTimeTarih.Size = new System.Drawing.Size(289, 20);
-            this.dateTimeTarih.TabIndex = 10;
-            // 
-            // btnYeni
-            // 
-            this.btnYeni.Location = new System.Drawing.Point(235, 264);
-            this.btnYeni.Name = "btnYeni";
-            this.btnYeni.Size = new System.Drawing.Size(93, 26);
-            this.btnYeni.TabIndex = 12;
-            this.btnYeni.Text = "Yeni";
-            this.btnYeni.UseVisualStyleBackColor = true;
-            this.btnYeni.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnKaydet
-            // 
-            this.btnKaydet.Location = new System.Drawing.Point(334, 264);
-            this.btnKaydet.Name = "btnKaydet";
-            this.btnKaydet.Size = new System.Drawing.Size(89, 25);
-            this.btnKaydet.TabIndex = 13;
-            this.btnKaydet.Text = "Kaydet";
-            this.btnKaydet.UseVisualStyleBackColor = true;
-            // 
-            // btnSil
-            // 
-            this.btnSil.Location = new System.Drawing.Point(429, 263);
-            this.btnSil.Name = "btnSil";
-            this.btnSil.Size = new System.Drawing.Size(83, 26);
-            this.btnSil.TabIndex = 14;
-            this.btnSil.Text = "Sil";
-            this.btnSil.UseVisualStyleBackColor = true;
-            this.btnSil.Click += new System.EventHandler(this.button3_Click);
+            this.gboxTabloGosterimi.Controls.Add(this.listView1);
+            this.gboxTabloGosterimi.Location = new System.Drawing.Point(25, 427);
+            this.gboxTabloGosterimi.Margin = new System.Windows.Forms.Padding(4);
+            this.gboxTabloGosterimi.Name = "gboxTabloGosterimi";
+            this.gboxTabloGosterimi.Padding = new System.Windows.Forms.Padding(4);
+            this.gboxTabloGosterimi.Size = new System.Drawing.Size(724, 190);
+            this.gboxTabloGosterimi.TabIndex = 1;
+            this.gboxTabloGosterimi.TabStop = false;
+            this.gboxTabloGosterimi.Text = "Tablo Gösterimi";
             // 
             // listView1
             // 
@@ -186,9 +207,10 @@
             this.columnHeader3,
             this.columnHeader4,
             this.columnHeader5});
-            this.listView1.Location = new System.Drawing.Point(3, 21);
+            this.listView1.Location = new System.Drawing.Point(4, 26);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(528, 126);
+            this.listView1.Size = new System.Drawing.Size(703, 154);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -218,13 +240,33 @@
             this.columnHeader5.Text = "Yorum";
             this.columnHeader5.Width = 185;
             // 
+            // txtGünNo
+            // 
+            this.txtGünNo.Enabled = false;
+            this.txtGünNo.Location = new System.Drawing.Point(156, 31);
+            this.txtGünNo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGünNo.Name = "txtGünNo";
+            this.txtGünNo.Size = new System.Drawing.Size(384, 22);
+            this.txtGünNo.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Özel Gün No :";
+            // 
             // frmTanimOzelGunler
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 469);
+            this.ClientSize = new System.Drawing.Size(787, 620);
             this.Controls.Add(this.gboxTabloGosterimi);
             this.Controls.Add(this.gboxOzelGunTanimlari);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmTanimOzelGunler";
             this.Text = "frmTanimOzelGunler";
             this.Load += new System.EventHandler(this.frmTanimOzelGunler_Load);
@@ -243,9 +285,9 @@
         private System.Windows.Forms.Button btnKaydet;
         private System.Windows.Forms.Button btnYeni;
         private System.Windows.Forms.DateTimePicker dateTimeTarih;
-        private System.Windows.Forms.TextBox tboxYorum;
-        private System.Windows.Forms.TextBox tboxmesaj;
-        private System.Windows.Forms.MaskedTextBox TboxMailBasligi;
+        private System.Windows.Forms.TextBox txtYorum;
+        private System.Windows.Forms.TextBox txtMsj;
+        private System.Windows.Forms.MaskedTextBox txtBaslik;
         private System.Windows.Forms.Label lblYorum;
         private System.Windows.Forms.Label lblMesaj;
         private System.Windows.Forms.Label lblTarih;
@@ -256,5 +298,7 @@
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.MaskedTextBox txtGünNo;
+        private System.Windows.Forms.Label label1;
     }
 }
--- telekomAidatTakip/frmTanimOzelGunler.cs
+++ telekomAidatTakip/frmTanimOzelGunler.cs
@@ -19,7 +19,110 @@ namespace telekomAidatTakip
 
         private void frmTanimOzelGunler_Load(object sender, EventArgs e)
         {
+            tabloDoldur();
+        }
 
+        private void tabloDoldur()
+        {
+            Database db = new Database();
+            var data = db.DataOku("SELECT * FROM OzelGunler");
+
+            listView1.Items.Clear();
+
+            while (data.Read())
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = data["ozelGunNo"].ToString();
+                item.SubItems.Add(data["baslik"].ToString());
+                item.SubItems.Add(data["tarih"].ToString());
+                item.SubItems.Add(data["mesaj"].ToString());
+                item.SubItems.Add(data["yorum"].ToString());
+               
+                listView1.Items.Add(item);
+                //oluşturulan item liste eklenir
+            }
+
+            txtBaslik.Enabled = false;
+            txtMsj.Enabled = false;
+            txtYorum.Enabled = false;
+            btnSil.Enabled = false;
+            btnKaydet.Enabled = false;
+            btnYeni.Enabled = true;
+        }
+
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            if (btnYeni.Text == "Yeni") // butonun ismi "Yeni" ise ekleme sayfası oluşturulmalı
+            {
+                if (btnKaydet.Enabled) // yeni butonuna basıldığı sırada bir kayıt düzenleniyor ise bunu tespit edip, kayıt için soruyor
+                {
+                    DialogResult dialogResult = MessageBox.Show("Değişiklikleri kaydetmek istiyor musunuz?", "", MessageBoxButtons.YesNoCancel);
+                    if (dialogResult == DialogResult.Yes)
+                        btnKaydet_Click(this, null);
+                    else if (dialogResult == DialogResult.Cancel)
+                        return;
+                }
+
+                txtBaslik.Text = string.Empty;
+                txtMsj.Text = string.Empty;
+                txtYorum.Text = string.Empty;
+                txtBaslik.Enabled = true;
+                txtMsj.Enabled = true;
+                txtYorum.Enabled = true;
+                
+                btnYeni.Text = "Ekle";
+                btnKaydet.Enabled = false;
+                btnSil.Enabled = false;
+            }
+            else //butonun ismi Yeni değilse demekki yeni kayıt sayfasındayız
+            {
+
+                if (txtBaslik.Text != string.Empty && txtMsj.Text != string.Empty) //yeni kayıt eklemek için bu iki verinin boş olmaması gerekiyor
+                {
+                    DateTime dt = this.dateTimeTarih.Value.Date;
+                    Database db = new Database();
+                    
+                    System.Data.SqlClient.SqlParameter param = new System.Data.SqlClient.SqlParameter("@3", dt);
+                    db.Sorgu("insert into OzelGunler(baslik,mesaj,yorum,tarih) values (@0,@1,@2,@3)", param, txtBaslik.Text, txtMsj.Text, txtYorum.Text);
+
+                    txtBaslik.Enabled = false;
+                    txtMsj.Enabled = false;
+                    txtYorum.Enabled = false;
+                    btnYeni.Text = "Yeni";
+                    tabloDoldur();
+                    txtBaslik.Text = string.Empty;
+                    txtMsj.Text = string.Empty;
+                    txtYorum.Text = string.Empty;
+                    btnYeni.Focus(); //görsel amaçlı imiş
+                }
+                else
+                {
+                    MessageBox.Show("Başlık veya mesaj boş!");
+                }
+
+            }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            /*if (txtBaslik.Text != string.Empty && txtMsj.Text != string.Empty)
+            {
+                DateTime dt = this.dateTimeTarih.Value.Date;
+                Database db = new Database();
+                System.Data.SqlClient.SqlParameter param = new System.Data.SqlClient.SqlParameter("@3", dt);
+                db.Sorgu("update OzelGunler set baslik=@0, mesaj=@1, yorum=@2, tarih=@3 where ozelGunNo=@4", param, txtBaslik.Text, txtMsj.Text, txtYorum.Text, BURAYA OzelgunNo'yu NASIL YAZACAM YA :'D);
+
+                txtBaslik.Text = string.Empty;
+                txtMsj.Text = string.Empty;
+                txtYorum.Text = string.Empty;
+                txtBaslik.Enabled = false;
+                txtMsj.Enabled = false;
+                txtYorum.Enabled = false;
+
+                btnSil.Enabled = false;
+                listView1.Items.Clear();
+                tabloDoldur();
+            }*/
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -27,10 +130,7 @@ namespace telekomAidatTakip
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void label2_Click(object sender, EventArgs e)
         {
@@ -38,6 +138,21 @@ namespace telekomAidatTakip
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TboxMailBasligi_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void dateTarih_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
         {
 
         }
