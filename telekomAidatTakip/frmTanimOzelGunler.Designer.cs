7bd05a222874588eb2ac338325970b915711dd36
burcuppo <burcu.polatt@outlook.com>
2017-07-05 14:08:59 +0300

    başlangıç

--- telekomAidatTakip/frmTanimOzelGunler.Designer.cs
+++ telekomAidatTakip/frmTanimOzelGunler.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmTanimOzelGunler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 503);
+            this.Name = "frmTanimOzelGunler";
             this.Text = "frmTanimOzelGunler";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
