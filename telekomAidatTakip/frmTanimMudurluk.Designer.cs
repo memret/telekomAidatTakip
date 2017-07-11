0fbfec1c7067e3e903665d545dbf680a1773657a
Mehmet Emre TURHAN <dcfromlife.emre@gmail.com>
2017-07-11 13:45:35 +0300

    metroda değişiklik ufak

--- telekomAidatTakip/frmTanimMudurluk.Designer.cs
+++ telekomAidatTakip/frmTanimMudurluk.Designer.cs
@@ -36,12 +36,12 @@
             this.btnSil = new MetroFramework.Controls.MetroTile();
             this.btnKaydet = new MetroFramework.Controls.MetroTile();
             this.btnYeni = new MetroFramework.Controls.MetroTile();
-            this.label_il = new System.Windows.Forms.Label();
             this.listvMdr = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.SuspendLayout();
             // 
             // txtMdrKod
@@ -127,7 +127,7 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(30, 114);
+            this.metroLabel1.Location = new System.Drawing.Point(30, 110);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(71, 19);
             this.metroLabel1.TabIndex = 25;
@@ -136,7 +136,7 @@
             // labelMdrNo
             // 
             this.labelMdrNo.AutoSize = true;
-            this.labelMdrNo.Location = new System.Drawing.Point(30, 77);
+            this.labelMdrNo.Location = new System.Drawing.Point(30, 73);
             this.labelMdrNo.Name = "labelMdrNo";
             this.labelMdrNo.Size = new System.Drawing.Size(93, 19);
             this.labelMdrNo.TabIndex = 24;
@@ -184,15 +184,6 @@
             this.btnYeni.UseSelectable = true;
             this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
             // 
-            // label_il
-            // 
-            this.label_il.AutoSize = true;
-            this.label_il.Location = new System.Drawing.Point(30, 155);
-            this.label_il.Name = "label_il";
-            this.label_il.Size = new System.Drawing.Size(15, 13);
-            this.label_il.TabIndex = 20;
-            this.label_il.Text = "İl:";
-            // 
             // listvMdr
             // 
             this.listvMdr.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
@@ -232,6 +223,15 @@
             this.columnHeader4.Text = "İl";
             this.columnHeader4.Width = 112;
             // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(30, 147);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(18, 19);
+            this.metroLabel2.TabIndex = 25;
+            this.metroLabel2.Text = "İl:";
+            // 
             // frmTanimMudurluk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -241,12 +241,12 @@
             this.Controls.Add(this.txtMdrKod);
             this.Controls.Add(this.comboBox_il);
             this.Controls.Add(this.txtMdrAd);
+            this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
             this.Controls.Add(this.labelMdrNo);
             this.Controls.Add(this.btnSil);
             this.Controls.Add(this.btnKaydet);
             this.Controls.Add(this.btnYeni);
-            this.Controls.Add(this.label_il);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MinimumSize = new System.Drawing.Size(555, 516);
@@ -270,11 +270,11 @@
         private MetroFramework.Controls.MetroTile btnSil;
         private MetroFramework.Controls.MetroTile btnKaydet;
         private MetroFramework.Controls.MetroTile btnYeni;
-        private System.Windows.Forms.Label label_il;
         private System.Windows.Forms.ListView listvMdr;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
     }
 }
