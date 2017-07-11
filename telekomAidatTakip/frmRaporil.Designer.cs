0ce2e6c9cd62049523f1448d414fe18be6b5a0a6
burcuppo <burcu.polatt@outlook.com>
2017-07-11 11:50:44 +0300

    rapor il

--- telekomAidatTakip/frmRaporil.Designer.cs
+++ telekomAidatTakip/frmRaporil.Designer.cs
@@ -28,10 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gboxListelemeİslemi = new System.Windows.Forms.GroupBox();
-            this.btnYazdir = new System.Windows.Forms.Button();
-            this.btnEkranaListele = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRaporIl));
             this.gboxBulunanKayitlar = new System.Windows.Forms.GroupBox();
             this.listIl = new System.Windows.Forms.ListView();
             this.listIlNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -39,72 +36,27 @@
             this.ListAktif = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ListPasif = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.ListToplam = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.gboxListelemeİslemi.SuspendLayout();
+            this.btnEkranaListele = new MetroFramework.Controls.MetroTile();
+            this.btnYazdir = new MetroFramework.Controls.MetroTile();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gboxBulunanKayitlar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // gboxListelemeİslemi
-            // 
-            this.gboxListelemeİslemi.Controls.Add(this.btnYazdir);
-            this.gboxListelemeİslemi.Controls.Add(this.btnEkranaListele);
-            this.gboxListelemeİslemi.Controls.Add(this.label1);
-            this.gboxListelemeİslemi.Location = new System.Drawing.Point(9, 12);
-            this.gboxListelemeİslemi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gboxListelemeİslemi.Name = "gboxListelemeİslemi";
-            this.gboxListelemeİslemi.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gboxListelemeİslemi.Size = new System.Drawing.Size(846, 154);
-            this.gboxListelemeİslemi.TabIndex = 0;
-            this.gboxListelemeİslemi.TabStop = false;
-            this.gboxListelemeİslemi.Text = "Listeleme İşlemi";
-            // 
-            // btnYazdir
-            // 
-            this.btnYazdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.btnYazdir.Location = new System.Drawing.Point(716, 91);
-            this.btnYazdir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnYazdir.Name = "btnYazdir";
-            this.btnYazdir.Size = new System.Drawing.Size(117, 48);
-            this.btnYazdir.TabIndex = 2;
-            this.btnYazdir.Text = "Yazdır";
-            this.btnYazdir.UseVisualStyleBackColor = true;
-            this.btnYazdir.Click += new System.EventHandler(this.btnYazdir_Click);
-            // 
-            // btnEkranaListele
-            // 
-            this.btnEkranaListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.btnEkranaListele.Location = new System.Drawing.Point(519, 91);
-            this.btnEkranaListele.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnEkranaListele.Name = "btnEkranaListele";
-            this.btnEkranaListele.Size = new System.Drawing.Size(187, 48);
-            this.btnEkranaListele.TabIndex = 1;
-            this.btnEkranaListele.Text = "Ekrana Listele";
-            this.btnEkranaListele.UseVisualStyleBackColor = true;
-            this.btnEkranaListele.Click += new System.EventHandler(this.btnEkranaListele_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 69);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "İllere Göre Üye Dağılım Listesi";
             // 
             // gboxBulunanKayitlar
             // 
             this.gboxBulunanKayitlar.Controls.Add(this.listIl);
-            this.gboxBulunanKayitlar.Location = new System.Drawing.Point(9, 175);
-            this.gboxBulunanKayitlar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gboxBulunanKayitlar.Location = new System.Drawing.Point(12, 97);
             this.gboxBulunanKayitlar.Name = "gboxBulunanKayitlar";
-            this.gboxBulunanKayitlar.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gboxBulunanKayitlar.Size = new System.Drawing.Size(846, 429);
+            this.gboxBulunanKayitlar.Size = new System.Drawing.Size(455, 291);
             this.gboxBulunanKayitlar.TabIndex = 1;
             this.gboxBulunanKayitlar.TabStop = false;
             this.gboxBulunanKayitlar.Text = "Bulunan Kayıtlar";
             // 
             // listIl
             // 
+            this.listIl.BackColor = System.Drawing.SystemColors.ControlLight;
             this.listIl.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.listIlNo,
             this.listİlAdi,
@@ -113,13 +65,14 @@
             this.ListToplam});
             this.listIl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listIl.FullRowSelect = true;
-            this.listIl.Location = new System.Drawing.Point(4, 24);
-            this.listIl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listIl.Location = new System.Drawing.Point(3, 16);
             this.listIl.Name = "listIl";
-            this.listIl.Size = new System.Drawing.Size(838, 400);
+            this.listIl.Size = new System.Drawing.Size(449, 272);
             this.listIl.TabIndex = 0;
             this.listIl.UseCompatibleStateImageBehavior = false;
+            this.listIl.UseWaitCursor = true;
             this.listIl.View = System.Windows.Forms.View.Details;
+            this.listIl.VirtualMode = true;
             this.listIl.SelectedIndexChanged += new System.EventHandler(this.listIl_SelectedIndexChanged);
             this.listIl.DoubleClick += new System.EventHandler(this.listIl_DoubleClick);
             // 
@@ -148,30 +101,86 @@
             this.ListToplam.Text = "Toplam";
             this.ListToplam.Width = 98;
             // 
+            // btnEkranaListele
+            // 
+            this.btnEkranaListele.ActiveControl = null;
+            this.btnEkranaListele.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnEkranaListele.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEkranaListele.Location = new System.Drawing.Point(217, 55);
+            this.btnEkranaListele.Name = "btnEkranaListele";
+            this.btnEkranaListele.Size = new System.Drawing.Size(122, 36);
+            this.btnEkranaListele.TabIndex = 3;
+            this.btnEkranaListele.Text = "Ekrana Listele";
+            this.btnEkranaListele.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnEkranaListele.UseCustomBackColor = true;
+            this.btnEkranaListele.UseCustomForeColor = true;
+            this.btnEkranaListele.UseSelectable = true;
+            this.btnEkranaListele.UseStyleColors = true;
+            this.btnEkranaListele.UseTileImage = true;
+            this.btnEkranaListele.UseWaitCursor = true;
+            this.btnEkranaListele.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // btnYazdir
+            // 
+            this.btnYazdir.ActiveControl = null;
+            this.btnYazdir.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnYazdir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnYazdir.Location = new System.Drawing.Point(345, 55);
+            this.btnYazdir.Name = "btnYazdir";
+            this.btnYazdir.Size = new System.Drawing.Size(122, 36);
+            this.btnYazdir.TabIndex = 4;
+            this.btnYazdir.Text = "Yazdır";
+            this.btnYazdir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnYazdir.UseCustomBackColor = true;
+            this.btnYazdir.UseCustomForeColor = true;
+            this.btnYazdir.UseSelectable = true;
+            this.btnYazdir.UseStyleColors = true;
+            this.btnYazdir.UseTileImage = true;
+            this.btnYazdir.UseWaitCursor = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 61);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(178, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "İllere GöreÜye Dağılım Listesi";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(135, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 41);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // frmRaporIl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 614);
+            this.ClientSize = new System.Drawing.Size(480, 398);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnYazdir);
             this.Controls.Add(this.gboxBulunanKayitlar);
-            this.Controls.Add(this.gboxListelemeİslemi);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Controls.Add(this.btnEkranaListele);
+            this.Controls.Add(this.metroLabel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.Name = "frmRaporIl";
+            this.Style = MetroFramework.MetroColorStyle.Red;
             this.Text = "İl Raporları";
             this.Load += new System.EventHandler(this.frmRaporIl_Load);
-            this.gboxListelemeİslemi.ResumeLayout(false);
-            this.gboxListelemeİslemi.PerformLayout();
             this.gboxBulunanKayitlar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox gboxListelemeİslemi;
-        private System.Windows.Forms.Button btnYazdir;
-        private System.Windows.Forms.Button btnEkranaListele;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox gboxBulunanKayitlar;
         private System.Windows.Forms.ListView listIl;
         private System.Windows.Forms.ColumnHeader ListAktif;
@@ -179,5 +188,9 @@
         private System.Windows.Forms.ColumnHeader ListToplam;
         private System.Windows.Forms.ColumnHeader listIlNo;
         private System.Windows.Forms.ColumnHeader listİlAdi;
+        private MetroFramework.Controls.MetroTile btnEkranaListele;
+        private MetroFramework.Controls.MetroTile btnYazdir;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
