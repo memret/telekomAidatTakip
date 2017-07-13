48146cdaaaaf3d5913bf00ed9c7f310ea670ce9a
Mehmet Emre TURHAN <dcfromlife.emre@gmail.com>
2017-07-13 16:32:10 +0300

    bişi

---
+++ telekomAidatCronTaskAdder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.TaskScheduler;
+using System.Diagnostics;
+
+namespace telekomAidatCronTaskAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskService.Instance.AddTask("Telekom Aidat Cron", QuickTriggerType.Logon , Environment.CurrentDirectory + "\\telekomAidatTakipCron.exe");
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.Arguments = "/C choice /C Y /N /D Y /T 3 & Del " +
+                           Environment.CurrentDirectory + "\\telekomAidatCronAdder.exe";
+            Info.WindowStyle = ProcessWindowStyle.Hidden;
+            Info.CreateNoWindow = true;
+            Info.FileName = "cmd.exe";
+            Process.Start(Info);
+        }
+    }
+}
